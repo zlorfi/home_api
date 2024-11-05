702854fea9f1b7a1609adbc317bdb8dc9de73a3e
Michael Skrynski <zlorfi@gmail.com>
2024-11-05 15:18:39 +0100

    adding brightness to Elgato controller

--- app/controllers/elgato_controller.rb
+++ app/controllers/elgato_controller.rb
@@ -6,7 +6,8 @@ class ElgatoController < ApplicationController
 
   def light
     if params.key?(:lights_on) && [true, false].include?(params[:lights_on])
-      body = { 'lights': [{ 'temperature': 175, 'on': params[:lights_on] ? 1 : 0, 'brightness': 55 }]}
+      Rage.logger.debug("BRIGHTNESS #{params[:brightness] || 'not present'}")
+      body = { 'lights': [{ 'temperature': 175, 'on': params[:lights_on] ? 1 : 0, 'brightness': params[:brightness] || 55 }]}
       headers = { 'Content-Type': 'application/json' }
       response = HTTParty.put(URI("#{HOST}/lights"), body: body.to_json, headers: headers)
       #response = Fiber.await([Fiber.schedule { HTTParty.put(URI("#{HOST}/lights"), body: body.to_json, headers: headers) }])
@@ -18,6 +19,6 @@ class ElgatoController < ApplicationController
 
   def status
     response = HTTParty.get(URI("#{HOST}/lights"))
-    render json: { lights_on: response['lights'][0]['on'] == 1 ? true : false, metadata: @metadata }
+    render json: { lights_on: response['lights'][0]['on'] == 1 ? true : false, brightness: response['lights'][0]['brightness'], metadata: @metadata }
   end
 end
